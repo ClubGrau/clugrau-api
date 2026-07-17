54af01a5581914607a1fa003db1c4cff28b65ab5
Paulo Sergio <pauloserg861@gmail.com>
2026-07-17 09:44:46 -0300

    feat: ensure EmployeePoliciesService is called correctly and propagate errors

--- src/modules/employees/application/usecases/create-employee.usecase.ts
+++ src/modules/employees/application/usecases/create-employee.usecase.ts
@@ -1,8 +1,13 @@
 import { Employee } from '@modules/employees/domain/entities/Employee';
 import { PasswordNotMatchError } from '@modules/employees/domain/errors/employee.errors';
 import { EmployeeModel } from '@modules/employees/domain/models/employee.model';
+import { EmployeePoliciesService } from '@modules/employees/domain/services/employee-policies.service';
 
 export class CreateEmployeeUsecase {
+  constructor(
+    private readonly employeePoliciesService: EmployeePoliciesService,
+  ) {}
+
   async execute(params: EmployeeModel.CreateEmployeeDto): Promise<void> {
     const { password, passwordConfirmation } = params;
 
@@ -10,12 +15,16 @@ export class CreateEmployeeUsecase {
       throw new PasswordNotMatchError();
     }
 
-    Employee.create({
+    const candidateEmployee = Employee.create({
       name: params.name,
       email: params.email,
       role: params.role,
       nif: params.nif,
       password: params.password,
-    });
+    }).toJSON();
+
+    await this.employeePoliciesService.ensureEmailIsAvailable(
+      candidateEmployee.email,
+    );
   }
 }
