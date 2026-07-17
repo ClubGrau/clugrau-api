2b98fc61bb4f8c7c5f97ab5c4c10b2b09f691dde
Paulo Sergio <pauloserg861@gmail.com>
2026-07-16 23:34:28 -0300

    test: ensure EmployeePoliciesService calls FindEmployeeByEmail with correct value

--- src/modules/employees/domain/services/employee-policies.service.spec.ts
+++ src/modules/employees/domain/services/employee-policies.service.spec.ts
@@ -1,12 +1,21 @@
+import { FindEmployeeByEmailPort } from '@modules/employees/application/ports/outbound/find-employee-by-email.port';
 import { EmployeePoliciesService } from './employee-policies.service';
 
+const makeStubs = () => ({
+  findEmployeeByEmailStub: {
+    findByEmail: jest.fn().mockResolvedValue(null),
+  } satisfies FindEmployeeByEmailPort,
+});
+
 const makeSut = (): SutTypes => {
-  const sut = new EmployeePoliciesService();
-  return { sut };
+  const { findEmployeeByEmailStub } = makeStubs();
+  const sut = new EmployeePoliciesService(findEmployeeByEmailStub);
+  return { sut, findEmployeeByEmailStub };
 };
 
 type SutTypes = {
   sut: EmployeePoliciesService;
+  findEmployeeByEmailStub: FindEmployeeByEmailPort;
 };
 
 describe('EmployeePoliciesService', () => {
@@ -14,5 +23,13 @@ describe('EmployeePoliciesService', () => {
     const { sut } = makeSut();
     expect(sut).toBeDefined();
     expect(sut).toBeInstanceOf(EmployeePoliciesService);
+  });
+
+  it('should call findEmployeeByEmail with correct email', async () => {
+    const { sut, findEmployeeByEmailStub } = makeSut();
+    const email = 'test@example.com';
+    const findByEmailSpy = jest.spyOn(findEmployeeByEmailStub, 'findByEmail');
+    await sut.checkEmployee(email);
+    expect(findByEmailSpy).toHaveBeenCalledWith(email);
   });
 });
