2af8ff0477dd3d91e8ca82f097e1e3236024278a
Paulo Sergio <pauloserg861@gmail.com>
2026-07-17 19:09:15 -0300

    feat: ensure CreateEmployeeController return 400 if name is not provided

--- src/modules/employees/presentation/controllers/create-employee.controller.ts
+++ src/modules/employees/presentation/controllers/create-employee.controller.ts
@@ -1,1 +1,34 @@
-export class CreateEmployeeController {}
+import { EmployeeModel } from '@modules/employees/domain/models/employee.model';
+
+interface HttpResponse<T> {
+  statusCode: number;
+  body: T;
+}
+
+type HttpErrorBody = { error: string };
+type HttpSuccessBody<T> = { data: T };
+
+export class CreateEmployeeController {
+  handle(
+    request: EmployeeModel.CreateEmployeeDto,
+  ): HttpResponse<
+    HttpErrorBody | HttpSuccessBody<EmployeeModel.CreateEmployeeResultDto>
+  > {
+    if (!request.name) {
+      return {
+        statusCode: 400,
+        body: {
+          error: 'Missing param name',
+        },
+      };
+    }
+
+    // TODO: call CreateEmployeeUsecase and return { data: result }
+    return {
+      statusCode: 200,
+      body: {
+        data: { id: '' },
+      },
+    };
+  }
+}
