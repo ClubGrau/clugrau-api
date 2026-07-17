9c6fece98d4eada7452369efc637b758662cbb1c
Paulo Sergio <pauloserg861@gmail.com>
2026-07-17 16:32:21 -0300

    chore: add test setup for mongo with mongoose

---
+++ src/configs/database/mongoose/testables.ts
@@ -0,0 +1,10 @@
+export const makeChainableMock = <T>(returnValue?: T) => ({
+  countDocuments: jest.fn().mockReturnValue(returnValue),
+  findOne: jest.fn().mockReturnThis(),
+  find: jest.fn().mockReturnThis(),
+  skip: jest.fn().mockReturnThis(),
+  limit: jest.fn().mockReturnThis(),
+  select: jest.fn().mockReturnThis(),
+  create: jest.fn().mockReturnValue(returnValue),
+  lean: jest.fn().mockReturnValue(returnValue),
+});
