f7a7ec7bb3051e76bdb4a60d621b617aef5eb430
Paulo Sergio <pauloserg861@gmail.com>
2026-07-17 22:02:43 -0300

    chore: add compose to load database in container and set collect coverage from

--- jest.config.js
+++ jest.config.js
@@ -14,11 +14,17 @@ module.exports = {
     '!src/**/*.test.ts',
     '!src/main.ts',
     '!src/**/*.module.ts',
+    '!src/configs/**/*.ts',
+    '!src/app.ts',
+    '!src/shared/infrastructure/adapters/http/*.adapter.ts',
+    '!src/shared/infrastructure/adapters/http/*.routes.ts',
+    '!src/**/*.schema.ts',
   ],
   coverageDirectory: 'coverage',
   moduleNameMapper: {
     '^@modules/(.*)$': '<rootDir>/src/modules/$1',
     '^@shared/(.*)$': '<rootDir>/src/shared/$1',
-    '^@config/(.*)$': '<rootDir>/src/config/$1',
+    '^@configs/(.*)$': '<rootDir>/src/configs/$1',
+    '^@config/(.*)$': '<rootDir>/src/configs/$1',
   },
 };
