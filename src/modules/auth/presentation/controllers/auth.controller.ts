c1aa3c8c1860ffcd2564a5cf854fa1c0e5adf3c5
Paulo Sergio <pauloserg861@gmail.com>
2026-07-18 17:41:57 -0300

    feat: ensure AuthController return 400 if email is not provided

--- src/modules/auth/presentation/controllers/auth.controller.ts
+++ src/modules/auth/presentation/controllers/auth.controller.ts
@@ -1,1 +1,18 @@
-export class AuthController {}
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { MissingParamError } from '@shared/presentation/errors/missing-param.error';
+
+interface LoginDto {
+  email: string;
+  password: string;
+}
+
+export class AuthController {
+  handle(request: LoginDto): any {
+    if (!request.email) {
+      return {
+        statusCode: 400,
+        body: new MissingParamError('email'),
+      };
+    }
+  }
+}
