c9046a1d660406115420f2e261b303fe8efd8d48
Paulo Sergio <pauloserg861@gmail.com>
2026-07-18 22:34:11 -0300

    test: ensure LoginUseCase calls FindAuthenticableByEmail with correct values

--- src/modules/auth/application/usecases/login.usecase.spec.ts
+++ src/modules/auth/application/usecases/login.usecase.spec.ts
@@ -1,14 +1,31 @@
+import { FindAuthenticatableByEmailPort } from '../ports/outbound/find-authenticable-by-email.port';
 import { LoginUseCase } from './login.usecase';
 
+const makeStubs = () => ({
+  findAuthenticatableByEmailPortStub: {
+    findAuthenticatableByEmail: jest.fn().mockResolvedValue({
+      id: 'any_id',
+      name: 'John Doe',
+      email: 'any_email@example.com',
+      passwordHash: 'hashed_password',
+      isActive: true,
+      role: 'EMPLOYEE',
+    }),
+  } satisfies FindAuthenticatableByEmailPort,
+});
+
 const makeSut = (): SutTypes => {
-  const sut = new LoginUseCase();
+  const { findAuthenticatableByEmailPortStub } = makeStubs();
+  const sut = new LoginUseCase(findAuthenticatableByEmailPortStub);
   return {
     sut,
+    findAuthenticatableByEmailPortStub,
   };
 };
 
 type SutTypes = {
   sut: LoginUseCase;
+  findAuthenticatableByEmailPortStub: FindAuthenticatableByEmailPort;
 };
 
 describe('LoginUsecase', () => {
@@ -16,5 +33,20 @@ describe('LoginUsecase', () => {
     const { sut } = makeSut();
     expect(sut).toBeDefined();
     expect(sut).toBeInstanceOf(LoginUseCase);
+  });
+
+  it('should call FindAuthenticatableByEmailPort with correct values', async () => {
+    const { sut, findAuthenticatableByEmailPortStub } = makeSut();
+    const params = {
+      email: 'any_email@example.com',
+      password: 'any_password',
+    };
+
+    const findAuthenticatableByEmailSpy = jest.spyOn(
+      findAuthenticatableByEmailPortStub,
+      'findAuthenticatableByEmail',
+    );
+    await sut.execute(params);
+    expect(findAuthenticatableByEmailSpy).toHaveBeenCalledWith(params.email);
   });
 });
