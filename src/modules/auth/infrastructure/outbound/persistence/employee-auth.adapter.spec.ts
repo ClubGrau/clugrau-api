bd81c571152e83895dc9ddaa0f47c2ddb23e5eeb
Paulo Sergio <pauloserg861@gmail.com>
2026-07-18 23:16:52 -0300

    test: ensure EmployeeAuthAdapter return null if employee not found and employee mapped if ok

--- src/modules/auth/infrastructure/outbound/persistence/employee-auth.adapter.spec.ts
+++ src/modules/auth/infrastructure/outbound/persistence/employee-auth.adapter.spec.ts
@@ -1,9 +1,32 @@
+import mongoose from 'mongoose';
+import { makeChainableMock } from '@configs/database/mongoose/testables';
+import {
+  EmployeeDocument,
+  EmployeeMongooseModel,
+} from '@modules/employees/infrastructure/outbound/persistence/employee.schema';
 import { EmployeeAuthAdapter } from './employee-auth.adapter';
 
+const mockEmployee = {
+  _id: new mongoose.Types.ObjectId(),
+  name: 'John Doe',
+  email: 'john.doe@example.com',
+  role: 'MANAGER',
+  password: 'hashed_password',
+  nif: 123456789,
+  isActive: true,
+  createdAt: new Date('2024-01-01T00:00:00Z'),
+  deactivateAt: null,
+} as EmployeeDocument;
+
+const mongooseMocks = () => makeChainableMock(mockEmployee);
+
 const makeSut = () => {
-  const sut = new EmployeeAuthAdapter();
+  const employeeModelMock = mongooseMocks();
+  const mongooseDeps = employeeModelMock as unknown as EmployeeMongooseModel;
+  const sut = new EmployeeAuthAdapter(mongooseDeps);
   return {
     sut,
+    employeeModelMock,
   };
 };
 
@@ -12,5 +35,54 @@ describe('EmployeeAuthAdapter', () => {
     const { sut } = makeSut();
     expect(sut).toBeDefined();
     expect(sut).toBeInstanceOf(EmployeeAuthAdapter);
+  });
+
+  describe('findAuthenticatableByEmail', () => {
+    it('should findOne employee by email with a valid Mongoose query', async () => {
+      const { sut, employeeModelMock } = makeSut();
+
+      const employeeId = new mongoose.Types.ObjectId().toHexString();
+      const email = 'john.doe@example.com';
+
+      const findOneSpy = jest
+        .spyOn(employeeModelMock, 'findOne')
+        .mockReturnValueOnce({
+          lean: jest.fn().mockResolvedValueOnce({
+            _id: employeeId,
+            name: 'John Doe',
+            email: 'john.doe@example.com',
+            password: 'hashed_password',
+            role: 'MANAGER',
+            nif: 123456789,
+            isActive: true,
+            createdAt: new Date('2024-01-01T00:00:00Z'),
+            deactivateAt: null,
+          }),
+        });
+
+      const result = await sut.findAuthenticatableByEmail(email);
+      expect(findOneSpy).toHaveBeenCalledWith({ email });
+      expect(result).toEqual({
+        id: employeeId,
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        passwordHash: 'hashed_password',
+        role: 'MANAGER',
+        isActive: true,
+      });
+    });
+
+    it('should return null if no employee is found', async () => {
+      const { sut, employeeModelMock } = makeSut();
+
+      const email = 'nonexistent@example.com';
+
+      jest.spyOn(employeeModelMock, 'findOne').mockReturnValueOnce({
+        lean: jest.fn().mockResolvedValueOnce(null),
+      });
+
+      const result = await sut.findAuthenticatableByEmail(email);
+      expect(result).toBeNull();
+    });
   });
 });
