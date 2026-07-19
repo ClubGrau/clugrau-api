224a5035719cfd6f941f9751be751aa78416e74a
Paulo Sergio <pauloserg861@gmail.com>
2026-07-18 23:28:26 -0300

    feat: ensure LoginUseCase throw AuthenticationError if user is not found

--- src/modules/auth/application/usecases/login.usecase.ts
+++ src/modules/auth/application/usecases/login.usecase.ts
@@ -1,17 +1,22 @@
 import { FindAuthenticatableByEmailPort } from '@modules/auth/application/ports/outbound/find-authenticable-by-email.port';
+import { LoginDto, LoginResultDto } from '../dtos/login.dto';
+import { AuthenticationError } from '@modules/auth/domain/errors/auth.errors';
 
 export class LoginUseCase {
   constructor(
     private readonly findAuthenticatableByEmailPort: FindAuthenticatableByEmailPort,
   ) {}
 
-  async execute(params: {
-    email: string;
-    password: string;
-  }): Promise<{ token: string }> {
-    await this.findAuthenticatableByEmailPort.findAuthenticatableByEmail(
-      params.email,
-    );
+  async execute(params: LoginDto): Promise<LoginResultDto> {
+    const user =
+      await this.findAuthenticatableByEmailPort.findAuthenticatableByEmail(
+        params.email,
+      );
+
+    if (!user) {
+      throw new AuthenticationError();
+    }
+
     return { token: 'valid_token' };
   }
 }
